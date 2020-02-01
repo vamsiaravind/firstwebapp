0a119e811ad3583674c9b2c603d3703b6b263966
shankar5885 <34203987+shankar5885@users.noreply.github.com>
2020-02-01 13:20:47 +0530

    Update HomeController.cs

--- myFirstAzureWebApp/Controllers/HomeController.cs
+++ myFirstAzureWebApp/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace myFirstAzureWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My contact page.";
 
             return View();
         }
